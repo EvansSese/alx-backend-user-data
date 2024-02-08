cf42be47a96751abaee83d8fb3431dc7ab2df6d4
Evans Sese <sese_evans@yahoo.com>
2024-02-08 13:42:21 +0300

    [Feature] Task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,9 +2,11 @@
 """Filtered logger file"""
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """Function to filter data"""
     return re.sub(fr'({separator.join(fields)})'
                   fr'{separator}', redaction + separator, message)
