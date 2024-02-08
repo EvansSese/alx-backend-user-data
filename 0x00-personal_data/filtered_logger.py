83e7084e953954093b6331bd4f974bb7365f4ff8
Evans Sese <sese_evans@yahoo.com>
2024-02-08 13:56:28 +0300

    [Feature] Task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,8 +4,15 @@
 import re
 from typing import List
 
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Function to filter data"""
-    return re.sub('|'.join(map(re.escape, fields)), redaction, message)
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
