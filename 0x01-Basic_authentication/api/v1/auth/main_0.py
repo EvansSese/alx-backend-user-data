3eed38118e28295dc4d73ee9b417f3271c82eb61
Evans Sese <sese_evans@yahoo.com>
2024-02-13 17:17:35 +0300

    [Feature] Task 3

--- 0x01-Basic_authentication/api/v1/auth/main_0.py
+++ 0x01-Basic_authentication/api/v1/auth/main_0.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ Main 0
 """
-from api.v1.auth.auth import Auth
+import auth
 
-a = Auth()
+a = auth.Auth()
 
 print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
 print(a.authorization_header())
