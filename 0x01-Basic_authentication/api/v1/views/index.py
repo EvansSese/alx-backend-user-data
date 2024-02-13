e4bad288348ac1d66a7bcad1a8b67e4abe36d746
Evans Sese <sese_evans@yahoo.com>
2024-02-13 13:37:14 +0300

    [Feature] Task 1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,7 +27,7 @@ def stats() -> str:
 
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized_endpoint():
+def unauthorized():
     """ GET /api/v1/unauthorized
     Return:
       - Error 401
