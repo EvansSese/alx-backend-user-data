d75bb7ebc61de5db6efc602ddef1cd2667d338d7
Evans Sese <sese_evans@yahoo.com>
2024-02-13 13:31:43 +0300

    [Feature] Task 1

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -22,6 +22,7 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """ Unauthorized handler when trying to access unauthorized route"""
