e8a57c57b05ac33abed82867fd71cdc7bb8f3b18
Evans Sese <sese_evans@yahoo.com>
2024-02-13 18:25:37 +0300

    [Feature] Task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,12 @@ class BasicAuth(Auth):
     """
     Basic Auth class
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Function to extract base64 header"""
+        if (authorization_header is None or
+                not isinstance(authorization_header, str) or
+                not authorization_header.startswith("Basic ")):
+            return None
+
+        return authorization_header.split(' ', 1)[1]
