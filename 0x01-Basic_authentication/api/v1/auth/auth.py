0ee127da534b63af26aa1f45fb456dd1905a10c8
Evans Sese <sese_evans@yahoo.com>
2024-02-13 17:25:52 +0300

    [Feature] Task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,32 @@ class Auth:
         """
         Function to check if auth is required
         """
-        return False
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        len_path = len(path)
+        if len_path == 0:
+            return True
+
+        slash_path = True if path[len_path - 1] == '/' else False
+
+        tmp_path = path
+        if not slash_path:
+            tmp_path += '/'
+
+        for exc_path in excluded_paths:
+            len_exc = len(exc_path)
+            if len_exc == 0:
+                continue
+
+            if exc_path[len_exc - 1] != '*':
+                if tmp_path == exc_path:
+                    return False
+            else:
+                if exc_path[:-1] == path[:len_exc - 1]:
+                    return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
