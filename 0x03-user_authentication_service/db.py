0b36ab425a1d0497e2ed4646fca31c7fab5bff73
Evans Sese <sese_evans@yahoo.com>
2024-02-19 15:48:06 +0300

    [Feature] Task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 """DB module
 """
+from typing import Type
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -31,7 +35,21 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
+        """Function to add user into db"""
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs: str) -> Type[User]:
+        """Function to query for user by provided input"""
+        user_keys = ['id', 'email', 'hashed_password',
+                     'session_id', 'reset_token']
+        for key in kwargs.keys():
+            if key not in user_keys:
+                raise InvalidRequestError
+
+        result = self._session.query(User).filter_by(**kwargs).first()
+        if result is None:
+            raise NoResultFound
+        return result
