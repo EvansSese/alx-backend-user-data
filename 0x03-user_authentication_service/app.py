c51c1dda00b70eee8776b6536634b61c3abbd3a3
Evans Sese <sese_evans@yahoo.com>
2024-02-21 18:31:30 +0300

    [Feature] Task 8

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -15,23 +15,5 @@ def index():
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route('/users', methods=['POST'])
-def users():
-    """Register user if they don't exist"""
-    try:
-        email = request.form['email']
-        password = request.form['password']
-    except KeyError:
-        abort(400)
-
-    try:
-        user = AUTH.register_user(email, password)
-    except ValueError:
-        return jsonify({"message": "email already registered"}), 400
-
-    msg = {"email": email, "message": "user created"}
-    return jsonify(msg)
-
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -33,3 +33,18 @@ class Auth:
             raise ValueError(f'User {email} already exists')
 
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Check if the provided credentials are correct"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+
+        user_password = bytes(user.hashed_password)
+        encoded_password = password.encode()
+
+        if bcrypt.checkpw(encoded_password, user_password):
+            return True
+
+        return False
