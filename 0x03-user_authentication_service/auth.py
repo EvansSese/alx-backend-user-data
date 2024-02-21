5d67d974fd4da9f780bba0423b957c0fccde02ee
Evans Sese <sese_evans@yahoo.com>
2024-02-21 17:12:14 +0300

    [Fix] Auth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,7 +2,7 @@
 """Auth module
 """
 from bcrypt import hashpw, gensalt
-from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import NoResultFound
 
 from db import DB
 from user import User
