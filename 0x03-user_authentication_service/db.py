42473864f841ec317d506c5ddc8fad33c3e39109
Evans Sese <sese_evans@yahoo.com>
2024-02-21 13:04:46 +0300

    [Fix] Db module

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,14 +2,12 @@
 """DB module
 """
 from typing import Type
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
-
+from sqlalchemy.exc import NoResultFound
 from user import Base, User
 
 
