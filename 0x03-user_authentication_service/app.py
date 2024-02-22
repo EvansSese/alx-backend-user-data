5ba800534028932efc2a136455a5f1737588baef
Evans Sese <sese_evans@yahoo.com>
2024-02-22 17:18:04 +0300

    [Feature] Task 19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -87,5 +87,20 @@ def reset_password():
         abort(403)
 
 
+@app.route('/update_password', methods=['PUT'])
+def update_password():
+    """Updates the user's password"""
+    try:
+        email = request.form['email']
+        reset_token = request.form['reset_token']
+        new_password = request.form['new_password']
+    except KeyError:
+        abort(403)
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
