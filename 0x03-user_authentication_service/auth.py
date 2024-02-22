12e1b35c64b9b814e8dccc4f663d032994f7bac7
Evans Sese <sese_evans@yahoo.com>
2024-02-22 12:56:22 +0300

    [Feature] Task 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,10 +56,13 @@ class Auth:
 
     def create_session(self, email: str) -> str:
         """Creates a session id for a logged-in user"""
-        user = self._db.find_user_by(email=email)
-        user_session_id = str(_generate_uuid())
         try:
-            self._db.update_user(user.id, session_id=user_session_id)
-            return user_session_id
-        except ValueError:
+            user = self._db.find_user_by(email=email)
+            user_session_id = str(_generate_uuid())
+            try:
+                self._db.update_user(user.id, session_id=user_session_id)
+                return user_session_id
+            except ValueError:
+                pass
+        except NoResultFound:
             pass
