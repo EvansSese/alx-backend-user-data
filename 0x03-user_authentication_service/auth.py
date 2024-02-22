98c6bdf1a4ab3990e7e3d1da01cf6f2575b60a5a
Evans Sese <sese_evans@yahoo.com>
2024-02-22 15:44:35 +0300

    [Fix] Auth Module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -46,7 +46,7 @@ class Auth:
         except NoResultFound:
             return False
 
-        user_password = bytes(user.hashed_password)
+        user_password = bytes(user.hashed_password.encode('utf-8'))
         provided_password = _hash_password(password)
 
         if checkpw(provided_password, user_password):
