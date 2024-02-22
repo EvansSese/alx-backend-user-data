2968ee323a461cf19315e76f2db4d2c18db03a88
Evans Sese <sese_evans@yahoo.com>
2024-02-22 17:04:53 +0300

    [Feature] Task 17

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -73,5 +73,19 @@ def get_profile():
     return jsonify({"email": user.email})
 
 
+@app.route('/reset_password', methods=['POST'])
+def reset_password():
+    """Resets a user's password"""
+    try:
+        email = request.form['email']
+        try:
+            token = AUTH.get_reset_password_token(email)
+            return jsonify({"email": email, "reset_token": token}), 200
+        except ValueError:
+            abort(403)
+    except KeyError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
