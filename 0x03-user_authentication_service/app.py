f25faeea2cdc2cf85b94c645685825e842688c34
Evans Sese <sese_evans@yahoo.com>
2024-02-22 10:05:32 +0300

    [Fix] Flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """Flass App Module"""
 from flask import Flask, jsonify, request, abort
-from auth import Auth
 
-AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -14,23 +12,5 @@ def index():
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route('/users', methods=['POST'])
-def users():
-    """Register user if they don't exist"""
-    try:
-        email = request.form['email']
-        password = request.form['password']
-    except KeyError:
-        abort(400)
-
-    try:
-        user = AUTH.register_user(email, password)
-    except ValueError:
-        return jsonify({"message": "email already registered"}), 400
-
-    msg = {"email": email, "message": "user created"}
-    return jsonify(msg)
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port="5000")
