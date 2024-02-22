44bf56751582353724094b9f52b4416b7fa38604
Evans Sese <sese_evans@yahoo.com>
2024-02-22 15:09:06 +0300

    [Feature] Task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -32,5 +32,24 @@ def users():
     return jsonify(msg), 200
 
 
+@app.route('/sessions', methods=['POST'])
+def login():
+    """Function to log in user"""
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    _is_valid_login = AUTH.valid_login(email, password)
+    if _is_valid_login:
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
